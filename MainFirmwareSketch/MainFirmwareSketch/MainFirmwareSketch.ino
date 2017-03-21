a68b64197b9c120083e4845d87414ee814ac2e9a
mstukolov <maxim.stukolov@gmail.com>
2017-03-21 11:31:25 +0300

    init

---
+++ MainFirmwareSketch/MainFirmwareSketch/MainFirmwareSketch.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MainFirmwareSketch.ino
+ Created:	3/21/2017 11:30:40 AM
+ Author:	Maxim
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
